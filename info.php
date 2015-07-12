ccd304e5d9a9906a97023fb4372590921bfa15c7
growthIQ <bo@getgrowthiq.com>
2015-07-12 19:50:39 -0400

    Update info.php

--- info.php
+++ info.php
@@ -1,3 +1,5 @@
 <?php
-    phpinfo();
+$dir = dirname(__FILE__);
+echo "<p>Full path to this dir: " . $dir . "</p>";
+echo "<p>Full path to a .htpasswd file in this dir: " . $dir . "/.htmypassword" . "</p>";
 ?>
